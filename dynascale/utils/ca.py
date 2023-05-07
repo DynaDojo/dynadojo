4823da04be4ac2e354c5c47450cc05c39693c66c
FlyingWorkshop <loganbham@yahoo.com>
2023-05-07 16:54:27 -0700

    CA coloring!

--- dynascale/utils/ca.py
+++ dynascale/utils/ca.py
@@ -1,7 +1,8 @@
 import cellpylib as cpl
 import numpy as np
+from matplotlib.colors import ListedColormap
 
 
 def plot(grid: list[np.ndarray], labels: list[str]):
     grid = [x[0] for x in grid]
-    cpl.plot_multiple(grid, labels)
+    cpl.plot_multiple(grid, labels, vmin=0)
