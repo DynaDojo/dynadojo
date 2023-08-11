3cd930ec271d6f4dadd070452d59b3f805e3c1b4
tommybru <tbru@stanford.edu>
2023-08-10 22:08:34 -0700

    Competitive LV

---
+++ dynadojo/dynadojo/systems/clv.py
@@ -0,0 +1,131 @@
+from dynascale.abstractions import AbstractSystem
+
+from scipy.integrate import odeint, solve_ivp
+import scipy as scipy
+import numpy as np
+
+RNG = np.random.default_rng()
+
+
+"""
+Competitive Lotka Volterra, generalized to n-species
+
+Complexity
+-------
+self.embed_dim, controls number of species and rank(A) of interaction matrix
+
+Competitive interaction dynamics
+-------
+Interspecies have only positive weighted interactions, a[i][j] ≥ 0 for all i, j
+Intraspecies set to 1, a[i][i] = 1 for all i
+Growth rate for each species is positive, r[i] > 0 for all i
+Each derviate divided by species carrying capacity, K[i]
+
+With n ≥ 5 species, asymptotic behavior occurs, including a fixed point, a limit cycle, an n-torus, or attractors [Smale]
+
+More info: https://en.wikipedia.org/wiki/Competitive_Lotka%E2%80%93Volterra_equations
+
+"""
+class CLVSystem(AbstractSystem):
+    def __init__(self, latent_dim, embed_dim,
+                 minK=1, 
+                 maxK=1000, 
+                 noise_scale=0.0000001,
+                 IND_range=(0.1, 0.5), # prevent spawning extinct species
+                 OOD_range=(0.5, 0.9),
+                 seed=None):
+        super().__init__(latent_dim, embed_dim)
+
+        self._noise_scale = noise_scale
+        self.minK=minK, 
+        self.maxK=maxK, 
+        self._rng = np.random.default_rng(seed)
+
+        self.R = self._make_R() # Growth Rate
+        self.K = self._make_K(self.minK, self.maxK) # Carrying capacity per species
+
+        self.A = self._make_A()
+
+        self.IND_range = IND_range
+        self.OOD_range = OOD_range
+
+    def _make_R(self):
+        return np.random.normal(0.5, 0.1, (self._embed_dim)) # r[i] must be positive
+    
+    def _make_K(self, minK, maxK):
+        return np.random.uniform(minK, maxK, (self._embed_dim))
+
+    def _make_C(self, minC, maxC):
+            return np.random.uniform(minC, maxC, (self._embed_dim))
+
+    def is_invertible(self, a):
+        return a.shape[0] == a.shape[1] and np.linalg.matrix_rank(a) == a.shape[0]
+
+
+    def _make_A(self):
+        A = np.random.normal(1, 0.3, (self._embed_dim, self._embed_dim)) #inter-species is all positive in competitive
+        for i in range(self._embed_dim):
+            for j in range(self._embed_dim):
+                if i == j:
+                    A[i][j] = 1 #intra-species is all 1 in competitive
+
+        return A / self.K
+
+    @AbstractSystem.embed_dim.setter
+    def embed_dim(self, value):
+        self._embed_dim = value
+        self.R = self._make_R() # Growth Rate
+        self.K = self._make_K(self.minK, self.maxK) # Carrying capacity per species
+
+        self.A = self._make_A()
+
+    def make_init_conds(self, n: int, in_dist=True) -> np.ndarray:
+        x0 = []
+        for _ in range(n):
+            temp = []
+            for s in range(self._embed_dim):
+                if in_dist:
+                    number = int(np.random.uniform(self.IND_range[0] * self.K[s], self.IND_range[1] * self.K[s]))
+                else:
+                    number = int(np.random.uniform(self.OOD_range[0] * self.K[s], self.OOD_range[1] * self.K[s]))
+                temp.append(number)
+            x0.append(temp)
+
+        return x0
+
+
+    def make_data(self, init_conds: np.ndarray, control: np.ndarray, timesteps: int, noisy=False) -> np.ndarray:
+        data = []
+        time = np.linspace(0, 100, timesteps)
+
+        def dynamics(t, X, u):
+            i = np.argmin(np.abs(t - time))
+
+            if noisy:
+                noise = np.random.normal(0,0.01,(self.embed_dim))
+                dX = (X * self.R * ((1 - (self.A @ X))+noise)) 
+            else:
+                dX = (X * self.R * ((1 - (self.A @ X)))) 
+            return dX
+
+
+        sol = []
+        if control:
+            for x0, u in zip(init_conds, control):
+                sol = solve_ivp(dynamics, t_span=[0, 1], y0=x0, t_eval=time, dense_output=True, args=(u,))
+
+        else:
+            for x0 in init_conds:
+                u = np.zeros((timesteps, self.embed_dim))
+                sol = solve_ivp(dynamics, t_span=[0, 100], y0=x0, t_eval=time, dense_output=True, args=(u,))
+                data.append(sol.y)
+
+        data = np.transpose(np.array(data), axes=(0, 2, 1)) 
+        return data
+
+    def calc_error(self, x, y) -> float:
+        error = x - y
+        return np.mean(error ** 2) / self.embed_dim
+
+    def calc_control_cost(self, control: np.ndarray) -> float:
+        return np.linalg.norm(control, axis=(1, 2), ord=2)
---
+++ dynadojo/dynadojo/utils/lv.py
@@ -0,0 +1,102 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.collections import LineCollection
+from mpl_toolkits.mplot3d.art3d import Line3DCollection
+from sklearn.decomposition import PCA
+
+MAX_LINES = 30
+
+def _plot2d(trajs_grid, gridlabels: list = None):
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    for i, trajs in enumerate(trajs_grid):
+        if gridlabels is None:
+            line_collection = LineCollection(trajs, color=f"C{i}")
+        else:
+            line_collection = LineCollection(trajs, color=f"C{i}", label=gridlabels[i])
+        ax.add_collection(line_collection)
+        ax.scatter(trajs[:, 0, 0], trajs[:, 0, 1], color=f"C{i}", marker="o")
+    minima = trajs_grid.min(axis=(0, 1, 2))
+    maxima = trajs_grid.max(axis=(0, 1, 2))
+    ax.set_xlim(minima[0], maxima[0])
+    ax.set_ylim(minima[1], maxima[1])
+    if gridlabels is not None:
+        ax.legend()
+    return fig, ax
+
+
+def _plot3d(grid, fig, gridlabels: list = None):
+    ax = fig.add_subplot( projection='3d')
+    for i, trajs in enumerate(grid):
+        if gridlabels is None:
+            line_collection = Line3DCollection(trajs, color=f"C{i}")
+        else:
+            line_collection = Line3DCollection(trajs, color=f"C{i}", label=gridlabels[i])
+        ax.add_collection(line_collection)
+        ax.scatter(trajs[:, 0, 0], trajs[:, 0, 1], trajs[:, 0, 2], color=f"C{i}", marker="o")
+    minima = grid.min(axis=(0, 1, 2))
+    maxima = grid.max(axis=(0, 1, 2))
+    ax.set_xlim(minima[0], maxima[0])
+    ax.set_ylim(minima[1], maxima[1])
+    ax.set_zlim(minima[2], maxima[2])
+    if gridlabels is not None:
+        ax.legend()
+    plt.show()
+    
+    return fig, ax
+
+
+def _plot4d(trajs_grid, pca: PCA, gridlabels: list = None):
+    assert pca.n_components == 2 or pca.n_components == 3
+    trajs_grid = _apply_pca_to_grid(trajs_grid, pca)
+    if pca.n_components == 2:
+        return _plot2d(trajs_grid, gridlabels)
+    else:
+        return _plot3d(trajs_grid, gridlabels)
+
+
+def make_pca(trajs: np.ndarray, n_components=3):
+    dim = trajs.shape[-1]
+    X = trajs.reshape((-1, dim))
+    pca = PCA(n_components=n_components)
+    pca.fit(X)
+    return pca
+
+
+def _apply_pca_to_grid(trajs_grid, pca) -> np.ndarray:
+    dim = trajs_grid.shape[-1]
+    X = trajs_grid.reshape((-1, dim))
+    result = pca.transform(X)
+    target_shape = list(trajs_grid.shape)
+    target_shape[-1] = pca.n_components
+    result = result.reshape(target_shape)
+    return result
+
+
+def plot(grid: list[np.ndarray], target_dim: int = 3, max_lines=MAX_LINES, specieslabels: list[str] = None, gridlabels: list[str] = None):
+    grid = np.array([x[:max_lines] for x in grid])
+    dim = grid.shape[-1]
+    fig = plt.figure(figsize=(16,4))
+    posidx = 1
+    for dataset in grid:
+        pos = 100 + len(grid) * 10 + posidx
+        ax = fig.add_subplot(1, len(grid), posidx)
+        
+        for i in range(len(dataset[0][0])):
+            ydata = dataset[0][:, i]
+            if specieslabels:
+                ax.plot(ydata, label=specieslabels[i])
+                ax.legend()
+            else:
+                ax.plot(ydata)
+        posidx += 1
+                
+    assert target_dim <= dim
+    assert target_dim <= 3
+    if dim == 2:
+        return _plot2d(grid, gridlabels)
+    elif dim == 3 and target_dim == 3:
+        return _plot3d(grid, gridlabels)
+    else:
+        pca = make_pca(grid, n_components=target_dim)
+        return _plot4d(grid,  pca, gridlabels)
