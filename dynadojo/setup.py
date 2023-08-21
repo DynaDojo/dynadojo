43fa4ce600261070580b317b4810ead150df13c7
Logan Mondal Bhamidipaty <76822456+FlyingWorkshop@users.noreply.github.com>
2023-08-20 19:03:42 -0700

    updated reqs

--- dynadojo/setup.py
+++ dynadojo/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dynadojo",
-    version="0.1.0",
+    version="0.2.0",
     description="An extensible platform for benchmarking sample efficiency in dynamical system identification",
     url="https://github.com/FlyingWorkshop/DynaDojo",
     author="Logan Mondal Bhamidipaty",
