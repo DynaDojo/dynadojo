f3b8676e19ae0a71d77763dbc6f0b8473327b0de
tommybru <tbru@stanford.edu>
2023-08-22 18:28:27 -0700

    Update __init__.py

--- dynadojo/__init__.py
+++ dynadojo/__init__.py
@@ -1,6 +1,6 @@
 """An extensible platform for benchmarking sample efficiency in dynamical system identification"""
 
-__version__ = "0.3.0"
+__version__ = "0.2.0"
 
 __author__ = "Logan Mondal Bhamidipaty, Tommy Anthony Bruzzese, Max Kanwal, Rami Ratl Mrad"
 __copyright__ = "Copyright 2023"
