0d1824ddbafaea3907b815aee3fc0e398bc44a7b
Caryn Tran <caryn.tran@berkeley.edu>
2023-10-01 20:27:38 -0500

    reorganized and clean up packages

--- src/dynadojo/__init__.py
+++ src/dynadojo/__init__.py
@@ -1,10 +1,5 @@
 """An extensible platform for benchmarking sample efficiency in dynamical system identification"""
 
-__version__ = "0.2.5"
-
-__copyright__ = "Copyright 2023"
-__license__ = "MIT"
-
 from . import systems
 from . import utils
 from . import baselines
