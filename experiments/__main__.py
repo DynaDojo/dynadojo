0a7f9449b8b845f4c62e109f0d99e61d908f3a64
Caryn Tran <caryn.tran@berkeley.edu>
2023-10-24 00:19:28 -0700

    fixed plotting

--- experiments/__main__.py
+++ experiments/__main__.py
@@ -1,8 +1,16 @@
 import argparse
 import dynadojo as dd
 print("hello")
+
+# # Accept command line arguments
+parser = argparse.ArgumentParser(description='Run experiments')
+parser.add_argument('--test', type=int, default=0, help='0=FixedComplexity, 1=FixedTrain, 2=FixedError')
+# add argument for test id in [1,2,3]
+args = parser.parse_args()
+
 # dd.testing.test_system("dynadojo.systems.lds", seed = 10, reps=2, eval_reps=None, test_ids=[0, 1,2])
-dd.testing.test_system("dynadojo.systems.lds", seed = 2000, reps=20, eval_reps=[], eval_L=[10], test_ids=[2],
+dd.testing.test_system("dynadojo.systems.lds", seed = 2000, reps=20, eval_reps=[], eval_L=[], test_ids=[args.test],
+    plot = True,
     # model_kwargs={"seed" : 160033707},
     # system_kwargs={"seed" : 2144784513},
 )
@@ -11,7 +19,3 @@ dd.testing.test_system("dynadojo.systems.lds", seed = 2000, reps=20, eval_reps=[
 # Should not run anything because of eval_L out of range
 # dd.testing.test_system("dynadojo.systems.lds", seed = 10, reps=2, eval_reps=[0], eval_L=[5] , test_ids=[2])
 
-# # Accept command line arguments
-# parser = argparse.ArgumentParser(description='Run experiments')
-# parser.add_argument('--system', type=str, default="dynadojo.systems.lds", help='system module')
-# parser.add_argument('--n', type=int, nargs='+', default=[1, 10], help='n')
