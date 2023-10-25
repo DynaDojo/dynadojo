cf427d8216e0593bbfcc71cdaa47da7c541498c0
Caryn Tran <caryn.tran@berkeley.edu>
2023-10-25 03:55:38 -0700

    cmdline

--- experiments/__main__.py
+++ experiments/__main__.py
@@ -5,7 +5,7 @@ Arguments:
     --system: which system to run, short name, see params.py system_dict
     --challenge: which challenge to run, one of ["fc", "fts", "fe"]
     --total_nodes: how many machines to run on (default 1, for running locally)
-    --node: which node is being run, [0, nodes-1], default None which runs the whole challenge
+    --node: which node is being run, [1, total_nodes], default None which runs the whole challenge
     --output_dir: where to save outputs
     --plot: whether to plot the results OR to run the challenge
 Usage:
@@ -26,7 +26,7 @@ parser.add_argument('--model', type=str, default='lr', help='Specify which model
 parser.add_argument('--system', type=str, default='lds', choices=system_dict.keys(), help='Specify which system to run')
 parser.add_argument('--challenge', type=str, default="fc", choices=["fc", "fts", "fe"], help='Specify which challenge to run')
 parser.add_argument('--total_nodes', type=int, default=1, help='how many machines to run on')
-parser.add_argument('--node', type=int, default=None, help='which node is being run in [0, nodes-1], if None, run on splits')
+parser.add_argument('--node', type=int, default=None, help='which node is being run in [1, total_nodes], if None, run on splits')
 parser.add_argument('--output_dir', type=str, default="experiments/outputs", help='where to save outputs')
 parser.add_argument('--plot', default=False, action='store_true', help='whether to plot results')
 # add argument for test id in [1,2,3]
@@ -52,7 +52,7 @@ if args.plot:
 else:
     # if node is specified, run that split
     if args.node is not None and args.total_nodes > 1:
-        assert args.node >= 0 and args.node < args.total_nodes, "node must be in [0, nodes-1]"
+        assert args.node >= 1 and args.node <= args.total_nodes, "node must be in [1, total_nodes]"
         max_splits = min(args.total_nodes, get_max_splits(s = args.system, m = args.model, challenge_cls = challenge_cls))
 
         if args.node < max_splits:
@@ -60,7 +60,7 @@ else:
                 s =args.system,
                 m = args.model,
                 output_dir=args.output_dir, 
-                split=(args.node+1, max_splits),
+                split=(args.node, max_splits),
                 challenge_cls=challenge_cls,
         )
     else: # run the whole challenge
