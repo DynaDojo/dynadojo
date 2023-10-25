2ba525b25d0659cc78c7e9d5554700dca3414466
Caryn Tran <caryn.tran@berkeley.edu>
2023-10-24 21:33:59 -0700

    sbatch file and experiment cmd line options

--- experiments/__main__.py
+++ experiments/__main__.py
@@ -11,11 +11,14 @@ Usage:
 """
 
 import argparse
+from .params import model_dict, system_dict
 from .main import run_challenge, make_plots, get_max_splits
 from dynadojo.challenges import  FixedError, FixedComplexity, FixedTrainSize
 
 # # Accept command line arguments
 parser = argparse.ArgumentParser(description='Run experiments')
+parser.add_argument('--model', type=str, default='lr', choice=model_dict.keys(), help='Specify which model to run')
+parser.add_argument('--system', type=str, default='lds', choice=system_dict.keys(), help='Specify which system to run')
 parser.add_argument('--challenge', type=str, default="fc", choices=["fc", "fts", "fe"], help='Specify which challenge to run')
 parser.add_argument('--total_nodes', type=int, default=1, help='how many machines to run on')
 parser.add_argument('--node', type=int, default=None, help='which node is being run in [0, nodes-1], if None, run on splits')
@@ -37,28 +40,28 @@ else:
 
 if args.plot:
     make_plots(
-        s ="lds",
-        m = "lr",
+        s = args.system,
+        m = args.model,
         output_dir=args.output_dir,
         challenge_cls = challenge_cls)
 else:
     # if node is specified, run that split
     if args.node is not None and args.total_nodes > 1:
         assert args.node >= 0 and args.node < args.total_nodes, "node must be in [0, nodes-1]"
-        max_splits = min(args.total_nodes, get_max_splits(s ="lds", m = "lr", challenge_cls = challenge_cls))
+        max_splits = min(args.total_nodes, get_max_splits(s = args.system, m = args.model, challenge_cls = challenge_cls))
 
         if args.node < max_splits:
             run_challenge(
-                s ="lds",
-                m = "lr",
+                s =args.system,
+                m = args.model,
                 output_dir=args.output_dir, 
                 split=(args.node+1, max_splits),
                 challenge_cls=challenge_cls,
         )
     else: # run the whole challenge
         run_challenge(
-            s ="lds",
-            m = "lr",
+            s = args.system,
+            m = args.model,
             output_dir=args.output_dir, 
             challenge_cls=challenge_cls,
         )
