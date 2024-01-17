d3747338ea19cec7076ea124e03fcc72202bd110
tommybru <tbru@stanford.edu>
2024-01-17 12:05:29 -0800

    Docs for LV

--- src/dynadojo/systems/lv/competitive.py
+++ src/dynadojo/systems/lv/competitive.py
@@ -6,35 +6,77 @@ from scipy.integrate import solve_ivp
 
 from ...abstractions import AbstractSystem
 
-"""
-Complexity
--------
-self.latent_dim, controls number of species and rank(A) of interaction matrix
-
-Competitive interaction dynamics
--------
-Interspecies have only positive weighted interactions, a[i][j] ≥ 0 for all i, j
-Intraspecies set to 1, a[i][i] = 1 for all i
-Growth rate for each species is positive, r[i] > 0 for all i
-Each derviate divided by species carrying capacity, K[i]
-
-With n ≥ 5 species, asymptotic behavior occurs, including a fixed point, a limit cycle, an n-torus, or attractors [Smale]
-
-More info: https://en.wikipedia.org/wiki/Competitive_Lotka%E2%80%93Volterra_equations
-
-"""
-
-
 class CompetitiveLVSystem(AbstractSystem):
+    """
+    Competitive Lotka Volterra, generalized to n-species
+    
+    Interspecies interaction have only positive weighted interactions, a[i][j] ≥ 0 for all i, j
+    Intraspecies interaction set to 1, a[i][i] = 1 for all i
+    Growth rate for each species is positive, r[i] > 0 for all i
+    Each derviate divided by species carrying capacity, K[i]
+
+    With n ≥ 5 species, asymptotic behavior occurs, including a fixed point, a limit cycle, an n-torus, or attractors [Smale]
+
+    More info: https://en.wikipedia.org/wiki/Competitive_Lotka%E2%80%93Volterra_equations
+
+    Example
+    ---------
+    >>> from dynadojo.systems.lv import CompetitiveLVSystem
+    >>> from dynadojo.wrappers import SystemChecker
+    >>> from dynadojo.utils.lv import plot
+    >>> latent_dim = [5]
+    >>> embed_dim = 5
+    >>> timesteps = 100
+    >>> n = 10
+    >>> system = SystemChecker(CompetitiveLVSystem(dim, embed_dim))
+    >>> x0 = system.make_init_conds(n=n)
+    >>> x = system.make_data(x0, timesteps=timesteps)
+    >>> plot([x], target_dim=2, specieslabels=["black bear", "salmon", "eagle", "lion", "dolphin", "tiger"], gridlabels=["IND", "IND-noisy", "OOD"], max_lines=100)
+
+    .. image:: ../_images/competitive.png
+
+    """
+
     def __init__(self, latent_dim=2, embed_dim=2,
                  minK=1,
                  maxK=10,
-                 noise_scale=0.01,
-                 IND_range=(0.1, 0.5),  # prevent spawning extinct species
+                 IND_range=(0.1, 0.5),
                  OOD_range=(0.5, 0.9),
-                 R_range=(0.0, 0.5),
-                 interspecies_range=(0, 1),
+                 R_dist=(0.0, 0.5),
+                 inter_dist=(0, 1),
+                 noise_scale=0.01,
                  seed=None):
+        """
+        Initializes a CompetitiveLVSystem instance.
+
+        Parameters
+        -------------
+        latent_dim : int
+            The number of species, the rank(A) of interaction matrix
+        embed_dim : int
+            Must be the same as latent_dim
+
+        minK : int
+            Minimum carrying capacity for all species
+        maxK : int
+            Minimum carrying capacity for all species
+        IND_range : tuple
+            In-distribution range of starting population numbers. The range is multipled by maxK and randomly sampled from.
+            The numbers should be greater than 0 to prevent spawing extinct species.
+        OOD_range : tuple
+            Out-of-distribution range of starting population numbers. The range is multipled by maxK and randomly sampled from.
+            The numbers should be greater than 0 to prevent spawing extinct species.
+        R_dist : tuple
+            Growth rates. Mu and standard deviation (spread or “width”) for normal distribution. 
+        inter_dist : tuple
+            Interspecies interaction distribution. Mu and standard deviation (spread or “width”) for normal distribution. 
+            Fixed to be all positive.
+        noise_scale : float
+            Normal noise is added per timestep to a solution. Standard deviation (spread or “width”) of the distribution.
+            Must be non-negative.
+        seed : int or None
+            Seed for random number generation.
+        """
         super().__init__(latent_dim, embed_dim, seed)
 
         assert embed_dim == latent_dim
@@ -44,8 +86,8 @@ class CompetitiveLVSystem(AbstractSystem):
         self.maxK = maxK,
         self._rng = np.random.default_rng(seed)
 
-        self.R_range = R_range
-        self.interspecies_range = interspecies_range
+        self.R_dist = R_dist
+        self.inter_dist = inter_dist
 
         self.R = self._make_R()  # Growth Rate
         self.K = self._make_K(self.minK, self.maxK)  # Carrying capacity
@@ -57,7 +99,7 @@ class CompetitiveLVSystem(AbstractSystem):
 
     def _make_R(self):
         # r[i] must be positive
-        return np.abs(self._rng.normal(self.R_range[0], self.R_range[1], (self._latent_dim)))
+        return np.abs(self._rng.normal(self.R_dist[0], self.R_dist[1], (self._latent_dim)))
 
     def _make_K(self, minK, maxK):
         return self._rng.uniform(minK, maxK, (self._latent_dim))
@@ -65,7 +107,7 @@ class CompetitiveLVSystem(AbstractSystem):
     def _make_A(self):
         # inter-species is all positive in competitive
         A = np.abs(self._rng.normal(
-            self.interspecies_range[0], self.interspecies_range[1], (self._latent_dim, self._latent_dim)))
+            self.inter_dist[0], self.inter_dist[1], (self._latent_dim, self._latent_dim)))
         for i in range(self._latent_dim):
             for j in range(self._latent_dim):
                 if i == j:
