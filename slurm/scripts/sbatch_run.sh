6bf1a0d1a5dcf7a23bcc86884132c72c622e0f9a
Max Kanwal <mkanwal@sh02-01n54.int>
2024-01-18 08:03:37 -0800

    run helper script yay

--- slurm/scripts/sbatch_run.sh
+++ slurm/scripts/sbatch_run.sh
@@ -11,29 +11,32 @@ read -p "System: " system
 read -p "Algorithm: " algo
 
 # Find potentially matching params files
-base_dir = $DD_SCRATCH_FOLDER/$DD_OUTPUT_FOLDER
-parent_dir = $base_dir/$challenge/$system
+base_dir=$DD_SCRATCH_DIR/$DD_OUTPUT_DIR
+parent_dir=$base_dir/$challenge/$system
 # List all subdirectories of parent dir
-subdirs = $(find $parent_dir -maxdepth 1 -type d)
+subdirs=$(find $parent_dir -maxdepth 1 -type d)
+# Filter only folders that match the algo $challenge_$system_$algo*
+subdirs=$(echo $subdirs | grep $challenge_$system_$algo)
 # Find all params.json files in subdirs
-params_files = $(find $subdirs -name "params.json")
+params_files=$(find $subdirs -name "params.json")
 # Replace base_dir from params_files with "experiments/outputs"
-params_files = $(echo $params_files | sed "s|$base_dir|experiments/outputs|g")
+options=( $(echo $params_files | sed "s|$base_dir|experiments/outputs|g") )
+
+# Exit if no options found
+if [ ${#options[@]} -eq 0 ]; then
+    echo "No params files found in $parent_dir"
+    return 0 2> /dev/null || exit 0
+fi
+
 # Print out params files as a numbered list for user to select from
 echo "Found the following params files:"
-for i in "${!params_files[@]}"; do 
-    echo "$i: ${params_files[$i]}"
+select opt in "${options[@]}"
+do
+    echo "Selected params file: $opt"
+    break
 done
 
-
-# Ask user to select params file & make sure it is a valid selection
-echo ""
-read -p "Select params file: " params_file_index
-while [[ ! $params_file_index =~ ^[0-9]+$ ]] || [[ $params_file_index -ge ${#params_files[@]} ]]; do
-    echo "Invalid selection. Try again."
-    read -p "Select params file: " params_file_index
-done
-params_file = ${params_files[$params_file_index]}
+params_file = ${params_files[$opt]}
 echo "Selected params file: $params_file"
 
 # Ask if they want to supply a list of jobs to run
