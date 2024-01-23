2eb8e9166647000a9d328df2d63be805ad702863
Caryn Tran <caryn.tran@berkeley.edu>
2024-01-23 03:38:49 -0600

    making generic files

--- slurm/quest/env_example.sh
+++ slurm/quest/env_example.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Make a copy of this file in this folder (`dynadojo/slurm/quest`) and name it as `env.sh`.
+
 # QUEST SLURM SETTINGS
-ACCOUNT='-A p32141'
+MY_USER='<YOUR STUDENT ID>'           #edit this
+MY_ACCOUNT='<YOUR ACCOUNT>'           #edit this, to check which account(s) is(are) associated with your user, e.g. with sacctmgr list user $USER.
+
+ACCOUNT='-A ${MY_ACCOUNT}'
 PARTITION='--partition short'
-OUTPUT='-o /home/ctb3982/logs/out/%A_%a.out'
-ERROR='-e /home/ctb3982/logs/err/%A_%a.err'
+OUTPUT='-o /home/${MY_USER}/logs/out/%A_%a.out'
+ERROR='-e /home/${MY_USER}/logs/err/%A_%a.err'
 TIME='-t 0-2:30' # Maximum execution time (D-HH:MM)
 
 # ENVIRONMENT VARS
@@ -12,7 +17,7 @@ export DD_SINGULARITY_IMAGE_LOCATION=$HOME/simg                 #where you want 
 export DD_REPO_DIR=$HOME                                        #parent directory of where you cloned dynadojo
 export DD_SCRATCH_DIR=$HOME                                     #your scratch directory  
 export DD_OUTPUT_DIR=questput                                   #name of folder in scratch to put output 
-export DD_IMAGE_REPO=docker://carynbear/dynadojo:slurm       #docker image to pull
+export DD_IMAGE_REPO=docker://carynbear/dynadojo:slurm          #docker image to pull
 export DD_CLUSTER=quest                                         #cluster name
 export DD_SLURM_ARGS="$ACCOUNT $PARTITION $TIME"
 export DD_SLURM_SAVE="$OUTPUT $ERROR"
--- slurm/quest/help.md
+++ slurm/quest/help.md
@@ -2,6 +2,7 @@
 
 1. Connect to the cluster, preferably using VSCode remote connection
 2. Figure out [MaxJobs](https://stackoverflow.com/a/61587377) & remember it
+3. Make an `env.sh` file, see `env_example.sh`
 
 Start an interactive node session: `srun -c 1 --pty bash`
 Start an interactive singularity: 
@@ -11,11 +12,9 @@ Start an interactive singularity:
 This [website](https://slurm.schedmd.com/job_array.html) is helpful
 
 ```bash
-squeue -u ctb3982
+squeue -u <user-name>
 scancel <job_id>
 sbatch <sbatch_file> --array=1-100 -J <job-name>
 scancel -n <job-name>
-
-# To get report: smanage report --sacct --name=dynadojo
-# To get details: squeue -u ctb3982
+smanage report --sacct --name=dynadojo #get report
 ```
