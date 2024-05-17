1adc9701c788862201e148e5b037c32465333fd8
Can Gurkan <cangurkan@me.com>
2024-05-17 03:26:01 -0500

    Implemented early stopping for gru_rnn. Added gru_rnn to the experiments CLI. Added gru_rnn options to challenge demos.

--- experiments/keys.py
+++ experiments/keys.py
@@ -18,4 +18,5 @@ algo_dict = {
     "lr" : ("dynadojo.baselines.lr", "LinearRegression"),
     "dnn" : ("dynadojo.baselines.dnn", "DNN"),
     "sindy": ("dynadojo.baselines.sindy", "SINDy"),
+    "gru_rnn": ("dynadojo.baselines.gru_rnn", "GRU_RNN"),
 }
