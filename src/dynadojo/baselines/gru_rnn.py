4c5795e56edc4823b79a9603ff481e0aae470198
Can Gurkan <cangurkan@me.com>
2024-05-17 03:26:01 -0500

    Implemented early stopping for gru_rnn. Added gru_rnn to the experiments CLI. Added gru_rnn options to challenge demos.

--- src/dynadojo/baselines/gru_rnn.py
+++ src/dynadojo/baselines/gru_rnn.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
+from torch.utils.data import random_split
 
 from ..abstractions import AbstractAlgorithm
 
@@ -8,28 +9,26 @@ from ..abstractions import AbstractAlgorithm
 class GRU_RNN(AbstractAlgorithm):
     def __init__(self, embed_dim: int, timesteps: int, max_control_cost: float, lr=1e-3, **kwargs):
         super().__init__(embed_dim, timesteps, max_control_cost, **kwargs)
-        self.hidden_dim = 32 #128
-        self.num_layers = 5
+        if kwargs.get('seed',None):
+            torch.manual_seed(kwargs['seed'])
+        self.hidden_size = kwargs.get('hidden_size', 32) #128
+        self.num_layers = kwargs.get('num_layers', 5)
         self.lr = lr
-        self.gru = nn.GRU(embed_dim, self.hidden_dim, self.num_layers, batch_first=True)
-        self.fc = nn.Linear(self.hidden_dim, embed_dim)
+        self.gru = nn.GRU(embed_dim, self.hidden_size, self.num_layers, batch_first=True)
+        self.fc = nn.Linear(self.hidden_size, embed_dim)
         self.model = nn.ModuleList([self.gru, self.fc])
-        #self.model = nn.Sequential(nn.Linear(embed_dim, 32), nn.Softplus(), nn.Linear(32, embed_dim))
-        #self.model = GRUModel(embed_dim,hidden_size=self.hidden_dim,num_layers=self.num_layers)
-        #self.model = SimpleLSTM(input_size=embed_dim, hidden_size=self.hidden_dim, num_layers=self.num_layers)
 
     def forward(self, x):
-        #h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim)
-        #out, _ = self.gru(x, h0)
-        #out = out.reshape(out.shape[0], -1)
-        #out = self.fc(out)
         _, h_n = self.gru(x)
         out = self.fc(h_n[-1])
-        
-        return out #self.model(x)
+        return out
 
-    def fit(self, x: np.ndarray, epochs=5000, **kwargs):
+    def fit(self, x: np.ndarray, epochs=1000, **kwargs):
         state = torch.tensor(x, dtype=torch.float32)
+        early_stopping = kwargs.get('early_stopping', False)
+        if early_stopping:
+            early_stopper = EarlyStopper(patience=kwargs.get('patience', 100), min_delta=kwargs.get('min_delta', 10))
+            state, val_state = map(lambda x: x.dataset, random_split(state, [0.8,0.2]))
         batch_size = len(state)
 
         opt = torch.optim.Adam(self.model.parameters(), self.lr)
@@ -37,61 +36,70 @@ class GRU_RNN(AbstractAlgorithm):
 
         for epoch in range(epochs):
             opt.zero_grad()
-            pred_states = state[:, 0, :].unsqueeze(1)
-            #print(state.shape)
             loss = 0.0
             losses = []
-
-            #pred_states = self.model(state)
-            #print(pred_states.shape,state.shape)
-            #loss = lossMSE(pred_states, state)
+            if early_stopping:
+                val_loss = 0.0
 
             for t in range(self._timesteps - 1):
                 state_t = state[:, t, :]
                 state_t = torch.reshape(state_t, (batch_size, self.embed_dim)).unsqueeze(1)
-                #print('state_t: ', state_t.shape)
-                #self.x = self.x.reshape(self.x.size(0), 1, 28, 28).squeeze(1)
                 next_state = self.forward(state_t)
-                #next_state = self.model(state_t)
-                #print('next_state: ', next_state.shape)
-                #print('state[:, t+1, :]: ', state[:, t+1, :].shape)
-                #loss += lossMSE((next_state * (state[:, t, :].unsqueeze(1))), state[:, t+1, :].unsqueeze(1))
                 loss += lossMSE(next_state,(state[:, t+1, :]))
-                #loss += lossMSE(next_state,torch.reshape(state[:, t+1, :], (batch_size, self.embed_dim)).unsqueeze(1))
-
-            losses.append(loss.item())
-            if epoch % 10 == 0 and kwargs.get('verbose', False):
-                print(f"Epoch {epoch}, Loss {loss.item()}")
+                if early_stopping:
+                    val_state_t = val_state[:, t, :]
+                    val_state_t = torch.reshape(val_state_t, (len(val_state), self.embed_dim)).unsqueeze(1)
+                    val_next_state = self.forward(val_state_t)
+                    val_loss += lossMSE(val_next_state, (val_state[:, t+1, :]))
 
             loss.backward()
             opt.step()
+
+            losses.append(loss.item())
+            log_freq = kwargs.get('log_freq', 100)
+            if epoch % log_freq == 0 and kwargs.get('verbose', False):
+                if early_stopping:
+                    print(f"Epoch {epoch}, Loss {loss.item()}, Validation Loss {val_loss.item()}")
+                else:
+                    print(f"Epoch {epoch}, Loss {loss.item()}")
+            if early_stopping and early_stopper.early_stop(val_loss.item()):
+                break
 
         return losses
 
     def predict(self, x0: np.ndarray, timesteps: int, **kwargs) -> np.ndarray:
         state = torch.tensor(x0, dtype=torch.float32)
-        print(state.shape)
         pred = state.unsqueeze(1)
-        #pred = self.model(state.unsqueeze(1)) #.unsqueeze(1)
-        #print(pred.shape)
 
         for t in range(timesteps - 1):
             state_t = pred[:, -1, :].unsqueeze(1)
-            #print(state_t.shape)
-            #next_state = self.forward(state_t)
             next_state = self.forward(state_t).unsqueeze(1)
-            #next_state = self.model(state_t).unsqueeze(1)
-            #next_state = torch.reshape(next_state, (len(state), self.embed_dim)).unsqueeze(1)
-            #print(next_state.shape)
             pred = torch.cat((pred, next_state), dim=1)
-        #print(pred.shape)
+        
         return pred.detach().numpy()
 
 
+class EarlyStopper:
+    def __init__(self, patience=10, min_delta=1):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = float('inf')
 
-class GRUModel(nn.Module):
+    def early_stop(self, validation_loss):
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
+
+
+class _GRU(nn.Module):
     def __init__(self, embed_dim: int, hidden_size=32, num_layers=5):
-        super(GRUModel, self).__init__()
+        super(_GRU, self).__init__()
         self.hidden_size  = hidden_size
         self.num_layers = num_layers
         
@@ -106,15 +114,15 @@ class GRUModel(nn.Module):
         return out
     
 
-class SimpleLSTM(nn.Module):
-    def __init__(self, input_size=9, hidden_size=32, num_layers=5):
-        super(SimpleLSTM, self).__init__()
+class _LSTM(nn.Module):
+    def __init__(self, embed_dim: int, hidden_size=32, num_layers=5):
+        super(_LSTM, self).__init__()
         
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc1 = nn.Linear(hidden_size, input_size)
+        self.lstm = nn.LSTM(embed_dim, hidden_size, num_layers, batch_first=True)
+        self.fc1 = nn.Linear(hidden_size, embed_dim)
         
     def forward(self, x):
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
