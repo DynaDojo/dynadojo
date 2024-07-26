294f1190864e840edc1cfb84ce60e5b1503478b5
oreo07-cyber <71622711+oreo07-cyber@users.noreply.github.com>
2024-07-26 13:55:16 -0400

    refactor: experiments params to config (#118)
    * changed all params.json and params_file to config.json and config_file
    
    * changed all params.json and params_file to config.json and config_file. Made experiments config file and ran experiment. All works
    
    * changed last thing from save_params -> save_config
    
    ---------
    
    Co-authored-by: Caryn Tran <caryn.tran@berkeley.edu>

--- experiments/__main__.py
+++ experiments/__main__.py
@@ -4,14 +4,14 @@ Arguments for make:
     --algo: which algo, short name, see params.py algo_dict
     --system: which system, short name, see params.py system_dict
     --challenge: which challenge, one of ["fc", "fts", "fe"]
-    --output_dir: where to save params, default "experiments/outputs"
+    --output_dir: where to save config, default "experiments/outputs"
     --all: if True, make all params, default False
 Usage:
     python -m experiments make --challenge <challenge_key> --system <system_key> --algo <algo_key> --output_dir <output_dir>
     python -m experiments make --challenge fc --system lds --algo lr_5
 
 Arguments for run:
-    --params_file: which params file to run
+    --config_file: which config file to run
     --total_nodes: how many machines to run on (default 1, for running locally)
     --node: which node is being run, [1, total_nodes], default None which runs the whole challenge
     --output_dir: where to save results, default "experiments/outputs"
@@ -21,12 +21,12 @@ Arguments for run:
 Usage:
     python -m experiments \
         run \
-        --params_file experiments/outputs/fc/lds/fc_lds_lr_l=10/params.json \
+        --config_file experiments/outputs/fc/lds/fc_lds_lr_l=10/config.json \
         --node 2 --total_nodes 10 \
         --num_cpu_parallel -2 \
         --if_missing
 
-    python -m experiments run --num_cpu_parallel -2 --params_file experiments/outputs/fc/lds/fc_lds_lr_5_l=5/params.json 
+    python -m experiments run --num_cpu_parallel -2 --config_file experiments/outputs/fc/lds/fc_lds_lr_5_l=5/config.json 
     
 Arguments for plot:
     --data_dir: where to load results from
@@ -53,7 +53,7 @@ python -m experiments make --challenge fe --algo lr --system lds --output_dir="e
 import argparse
 import os
 from .utils import algo_dict, load_from_json, system_dict, challenge_dicts
-from .main import load_data, run_challenge, make_plots, save_params, prGreen, prPink
+from .main import load_data, run_challenge, make_plots, save_config, prGreen, prPink
 from dynadojo.challenges import  FixedError, FixedComplexity, FixedTrainSize
 
 
@@ -69,11 +69,11 @@ scale_parser = subparsers.add_parser('scale', help='Temporary utility which resc
 make_parser.add_argument('--algo', type=str, default='lr', help='Specify which algo to run')
 make_parser.add_argument('--system', type=str, default='lds', choices=system_dict.keys(), help='Specify which system to run')
 make_parser.add_argument('--challenge', type=str, default="fc", choices=["fc", "fts", "fe"], help='Specify which challenge to run')
-make_parser.add_argument('--output_dir', type=str, default="experiments/outputs", help='where to save params')
+make_parser.add_argument('--output_dir', type=str, default="experiments/outputs", help='where to save config')
 make_parser.add_argument('--all', action='store_true', help='if True, make all params')
 make_parser.set_defaults(all=False)
 
-run_parser.add_argument('--params_file', type=str, help='what params file to run')
+run_parser.add_argument('--config_file', type=str, help='what config file to run')
 run_parser.add_argument('--node', type=int, default=None, help='which node is being run in [1, total_nodes], if None, run on splits')
 run_parser.add_argument('--total_nodes', type=int, default=1, help='how many machines to run on')
 run_parser.add_argument('--num_cpu_parallel', type=int, default=None, help='number of cpus to use for parallelization')
@@ -99,8 +99,8 @@ if args.command == 'make':
                     for a in chall_dict[s].keys():
                         if a != "default":
                             print(f"Making {c.__name__} {s} {a}")
-                            params_file, total_jobs = save_params(s, a, challenge_cls=c, output_dir=args.output_dir)
-                            prPink(f"{params_file} with {total_jobs} jobs")
+                            config_file, total_jobs = save_config(s, a, challenge_cls=c, output_dir=args.output_dir)
+                            prPink(f"{config_file} with {total_jobs} jobs")
     else:
         assert args.algo.split("_")[0] in algo_dict.keys(), f"algo {args.algo} must be in algo_dict"
         if args.challenge == "fc":
@@ -109,22 +109,22 @@ if args.command == 'make':
             challenge_cls = FixedTrainSize
         else:
             challenge_cls = FixedError
-        params_file, total_jobs = save_params(args.system, args.algo, challenge_cls, output_dir=args.output_dir)
-        prPink(f"{params_file} with {total_jobs} jobs")
+        config_file, total_jobs = save_config(args.system, args.algo, challenge_cls, output_dir=args.output_dir)
+        prPink(f"{config_file} with {total_jobs} jobs")
         if rest: #maybe parse more args
             args = program.parse_args(rest) 
             if args.command == 'run':
-                args.params_file = params_file
+                args.config_file = config_file
             else:
                 exit(0)
 
 if args.command == 'run':
-    assert args.params_file is not None, "must specify params file"
+    assert args.config_file is not None, "must specify config file"
     
     if args.if_missing:
-        params = load_from_json(args.params_file)
-        total_jobs = params["total_jobs"]
-        _, data = load_data(os.path.join(args.output_dir, params["folder_path"]))
+        config = load_from_json(args.config_file)
+        total_jobs = config["total_jobs"]
+        _, data = load_data(os.path.join(args.output_dir, config["folder_path"]))
         if data is None:
             prGreen("No previous jobs found.")
             args.jobs = None
@@ -140,7 +140,7 @@ if args.command == 'run':
     if args.node is not None and args.total_nodes > 1:
         assert args.node >= 1 and args.node <= args.total_nodes, f"{args.node=} must be between[1, {args.total_nodes=}]"
         run_challenge(
-            params_file_path=args.params_file,
+            config_file_path=args.config_file,
             output_dir=args.output_dir,
             split=(args.node, args.total_nodes),
             num_cpu_parallel=args.num_cpu_parallel,
@@ -149,7 +149,7 @@ if args.command == 'run':
     else: # run the whole challenge
         prGreen(f"Running {len(args.jobs.split(',')) if args.jobs else 'all'} jobs.")
         run_challenge(
-            params_file_path=args.params_file,
+            config_file_path=args.config_file,
             output_dir=args.output_dir,
             split=None,
             num_cpu_parallel=args.num_cpu_parallel,
@@ -165,12 +165,12 @@ elif args.command == 'plot':
     )
 
 elif args.command == 'check':
-    #must contain params.json
+    #must contain config.json
     assert args.data_dir is not None, "must specify data directory"
-    assert os.path.exists(args.data_dir + "/params.json"), f"params.json not found in {args.data_dir}"
+    assert os.path.exists(args.data_dir + "/config.json"), f"config.json not found in {args.data_dir}"
 
-    params = load_from_json(args.data_dir + "/params.json")
-    total_jobs = params["total_jobs"]
+    config = load_from_json(args.data_dir + "/config.json")
+    total_jobs = config["total_jobs"]
     _, data = load_data(args.data_dir)
     if data is None:
         completed_jobs = []
